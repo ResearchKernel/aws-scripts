cb4808c502fb644460db97c7a5b0cbf19a6d6b83
Prakritidev Verma <Prakritidevverma@gmail.com>
2019-01-29 13:19:29 +0530

    change import os

--- neo4j_relationship_creator.py
+++ neo4j_relationship_creator.py
@@ -1,7 +1,7 @@
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-import osax
+import os
 import pandas as pd 
 import gensim 
 from py2neo import Graph, Node, Relationship
