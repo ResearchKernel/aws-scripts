148d5a4f328875d1df826bd8aba94b53c81fdcf0
Prakritidev Verma <Prakritidevverma@gmail.com>
2019-01-29 13:35:25 +0530

    added main function and exceptional handling

--- node_creator.py
+++ node_creator.py
@@ -8,26 +8,37 @@ from py2neo import Graph, Node, Relationship
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-graph = Graph(password="1234")
 
-def neo_node_creator(arxiv_id):
+
+def neo_node_creator(arxiv_id, graph):
         cypher = graph.begin()
         print("Runing Cypher")
         cypher.run('create (id:paper {arxiv_id:"%s"})' % arxiv_id)
         print("yess,  running")
         cypher.commit() 
         print("commited again !!!")
-        print("Done ",arxiv_id)
+        print("Done ",arxiv_id)    
 
-def main_node_builder(conn):
+if __name__ == "__main__":
     pool = Pool(1)
+    try: 
+        graph = Graph(password="1234")
+    except Exception as e:
+        print(e)
     filenames_base_list = []
     for (dirpath, dirnames, filenames) in os.walk("data/pdf/"):
             filenames_base_list += [os.path.join(dirpath, file) for file in filenames]
     arxiv_id_filenames_base = [os.path.basename(i) for i in filenames_base_list[1:]]
     arxiv_id = [os.path.splitext(i)[0] for i in arxiv_id_filenames_base]
     print("done arxiv ID")
-    pool.map(neo_node_creator,arxiv_id)
-    pool.close()
-    pool.join()
-    conn.send(arxiv_id)
+    try:
+        pool.map(neo_node_creator,arxiv_id)
+    except Exception as e:
+        print(e)
+        pool.close()
+        pool.join()
+    finally:
+        pool.close()
+        pool.join()
+
+    
